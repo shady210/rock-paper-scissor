b14ffaa7859b0f930fe479df8eb214de3632d9c7
Shady <somdeepshadab1@gmail.com>
2022-08-19 16:43:14 +0530

    Added UI in Rock paper

--- rock.js
+++ rock.js
@@ -1,4 +1,70 @@
 
+var rockVal = null;
+var clicks = 0;
+let compCount = 0;
+let userCount = 0;
+let tieCount = 0;
+
+const elements =  Array.from(document.querySelectorAll('button'));
+console.log(elements);
+
+elements.forEach(btn=>{
+    btn.addEventListener('click',e=>{
+         clicks+=1
+         if(clicks <=5){
+            document.getElementById('selection').innerHTML = `You have selected`+' '+e.target.outerText+'!!';
+            rockVal = e.target.outerText;
+            let computerSelection =  getComputerChoice();
+            document.getElementById('compSelection').innerHTML = `Computer have selected` + ' '+computerSelection+'!!';
+            document.getElementById('winner').textContent = `Match is Inprogress`;
+            play(rockVal,computerSelection,clicks)
+         }
+         else{  
+              
+            setCount();
+            localStorage.removeItem('userCount');
+            localStorage.removeItem('computerCount');
+            localStorage.removeItem('tieCount');
+            alert(`Mate Game is Over`);
+            document.getElementById('rock').setAttribute('disabled',true)
+            document.getElementById('paper').setAttribute('disabled',true)
+            document.getElementById('scissor').setAttribute('disabled',true)
+            btn.removeEventListener("click",function(){
+                   
+            },false);
+         }
+         
+    })
+    
+})
+
+
+function setCount () {
+    localStorage.setItem('computerCount',compCount);
+    localStorage.setItem('userCount',userCount);
+    localStorage.setItem('tieCount',tieCount);
+    var retrievedCount1 = localStorage.getItem('computerCount');
+    var retrievedCount2 = localStorage.getItem('userCount');
+    var retrievedCount3 = localStorage.getItem('tieCount');
+
+    if(retrievedCount1>retrievedCount2 && retrievedCount1>=retrievedCount3)
+    {
+        document.getElementById('winner').textContent = `Computer is Winner!`;
+    }
+    else if (retrievedCount2>retrievedCount1 && retrievedCount2>=retrievedCount3)
+    {
+        document.getElementById('winner').textContent = `Hey Buddy you are the Winner!!`;
+    }
+    else{
+        document.getElementById('winner').textContent = `Match is tied`;
+    }
+}
+
+function restart()
+{
+    location.reload();
+}
+
 function getComputerChoice()
 {
    let number= Math.random()*3;
@@ -18,94 +84,75 @@ function getComputerChoice()
 }
 
 
-function play(playerSelection,computerSelection)
+function play(playerSelection,computerSelection,x)
 {
     let pl = playerSelection.charAt(0).toUpperCase();
     let player = pl.concat(playerSelection.slice(1));
 
     let com = computerSelection.charAt(0).toUpperCase()
     let computer= com.concat(computerSelection.slice(1));
-
-
     let result = calc(player,computer)
     
     if(result==='comp')
-    {   
-        return `You Lose! ${computer} beats ${player}`
+    {
+     
+        compCount+=1;
+        document.getElementById('compId').innerHTML = compCount;
+        console.log(`You Lose! ${computer} beats ${player}`,compCount)
     }
     else if(result==='user')
     {
-        return `You Won! ${player} beats ${computer}`
+        userCount+=1;
+        document.getElementById('scoreId').innerHTML = userCount;
+        console.log(`You Won! ${player} beats ${computer}`)
     }
     else
     {
-        return `Match is Tie!!`
+        tieCount+=1;
+        console.log(`Match is Tie!!`);
     }
-}
-
-function game()
-{
-    let count =0,count1=0,count2=0;
-    for(let i=0;i<5;i++)
-    {
-        let computerSelection = getComputerChoice();
-        let playerSelection = prompt('Enter your choice')
-        let res = play(playerSelection,computerSelection);
-        console.log(res)
-        if(res.includes('Won'))
-        {
-            count++;
-        }
-        else if (res.includes('Lose'))
-        {
-            count1++;
-        }
-        else
-        {
-            count2++;
-        }
-    }
-
-    console.log(count,count1,count2);
-    if(count > count1 && count >= count2)
-    {
-        return `Ultimately!!! You Won`
-    }
-    else if(count1>count && count1>=count2){
-        return `You Lose!!!`
-    }
-    else{
-        return `Match is a tie`
-    }
-    
 }
 
 function calc(x,y)
 {
+    let rockUrl = "https://www.wikihow.com/images/thumb/1/1e/Play-Rock%2C-Paper%2C-Scissors-Step-6-Version-4.jpg/aid42597-v4-728px-Play-Rock%2C-Paper%2C-Scissors-Step-6-Version-4.jpg.webp"
+    let paperUrl = "https://www.wikihow.com/images/thumb/4/49/Play-Rock%2C-Paper%2C-Scissors-Step-7-Version-4.jpg/aid42597-v4-728px-Play-Rock%2C-Paper%2C-Scissors-Step-7-Version-4.jpg.webp";
+    let scissorUrl = "https://www.wikihow.com/images/thumb/b/b2/Play-Rock%2C-Paper%2C-Scissors-Step-8-Version-4.jpg/aid42597-v4-728px-Play-Rock%2C-Paper%2C-Scissors-Step-8-Version-4.jpg.webp";
+
     if(x==='Rock')
     {
+        document.getElementById('user-choice').src = rockUrl;
+        console.log(document.getElementById('user-choice').src);
+
         if(y==='Paper')
         {
+            document.getElementById('comp-choice').src = paperUrl;
            return 'comp';
         }
 
         else if(y==='Scissor')
         {
+            document.getElementById('comp-choice').src = scissorUrl;
             return 'user';
         }
         else{
+            document.getElementById('comp-choice').src = rockUrl;
             return 'tie';
         }
     }
     else if(x==='Paper')
     {
+        document.getElementById('user-choice').src = paperUrl;
+
         if(y==='Rock')
         {
+            document.getElementById('comp-choice').src = rockUrl;
            return 'user';
         }
 
         else if(y==='Scissor')
         {
+            document.getElementById('comp-choice').src = scissorUrl;
             return 'comp';
         }
         else{
@@ -113,21 +160,31 @@ function calc(x,y)
         }
     }
     else{
+
+        document.getElementById('user-choice').src = scissorUrl;
         if(y==='Rock')
         {
+            document.getElementById('comp-choice').src = rockUrl;
            return 'comp';
         }
 
         else if(y==='Paper')
         {
+            document.getElementById('comp-choice').src = paperUrl;
             return 'user';
         }
         else{
+
+            document.getElementById('comp-choice').src = scissorUrl;
             return 'tie';
         }
     }
 }
-console.log(game());
+
+
+
+
+
 
 
 
